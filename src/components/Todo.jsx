952d959d0eb84a43166fcf8c6f6c55dbbdf8bc6c
Peter Osoo <posoo617@gmail.com>
2023-09-01 15:07:58 +0300

    styles

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react"
 
-function TodoApp() {
+const Todo = () => {
 	const [todos, setTodos] = useState([])
 	const [newTodo, setNewTodo] = useState("")
 
@@ -18,24 +18,41 @@ function TodoApp() {
 	}
 
 	return (
-		<div className="container">
-			<h1>To-Do App</h1>
-			<div>
+		<div className="container mt-5">
+			<h1 className="text-center">To-Do App</h1>
+			<div className="input-group mb-3">
 				<input
 					type="text"
+					className="form-control"
 					placeholder="Add a new task"
 					value={newTodo}
 					onChange={e => setNewTodo(e.target.value)}
 				/>
-				<button onClick={addTodo}>Add</button>
+				<div className="input-group-append">
+					<button
+						className="btn btn-success mx-3"
+						type="button"
+						onClick={addTodo}
+					>
+						Add
+					</button>
+				</div>
 			</div>
 
-			{/* display todo items  */}
 			<h3>Todo List</h3>
-			<ul>
+			<ul className="list-group">
 				{todos.map((todo, index) => (
-					<li key={index}>
-						{todo} <button onClick={() => removeTodo(index)}>Remove</button>
+					<li
+						key={index}
+						className="list-group-item d-flex justify-content-between align-items-center"
+					>
+						{todo}
+						<button
+							className="btn btn-danger btn-sm"
+							onClick={() => removeTodo(index)}
+						>
+							Remove
+						</button>
 					</li>
 				))}
 			</ul>
@@ -43,4 +60,4 @@ function TodoApp() {
 	)
 }
 
-export default TodoApp
+export default Todo
