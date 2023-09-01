07d376197f9ba7bd9fbb3a496b811bff6fae4c40
Peter Osoo <posoo617@gmail.com>
2023-09-01 11:44:45 +0300

    Todo

---
+++ src/components/Todo.jsx
@@ -0,0 +1,46 @@
+import { useState } from "react"
+
+function TodoApp() {
+	const [todos, setTodos] = useState([])
+	const [newTodo, setNewTodo] = useState("")
+
+	const addTodo = () => {
+		if (newTodo.trim() !== "") {
+			setTodos([...todos, newTodo])
+			setNewTodo("")
+		}
+	}
+
+	const removeTodo = index => {
+		const updatedTodos = [...todos]
+		updatedTodos.splice(index, 1)
+		setTodos(updatedTodos)
+	}
+
+	return (
+		<div>
+			<h1>To-Do App</h1>
+			<div>
+				<input
+					type="text"
+					placeholder="Add a new task"
+					value={newTodo}
+					onChange={e => setNewTodo(e.target.value)}
+				/>
+				<button onClick={addTodo}>Add</button>
+			</div>
+
+			{/* display todo items  */}
+			<h3>Todo List</h3>
+			<ul>
+				{todos.map((todo, index) => (
+					<li key={index}>
+						{todo} <button onClick={() => removeTodo(index)}>Remove</button>
+					</li>
+				))}
+			</ul>
+		</div>
+	)
+}
+
+export default TodoApp
